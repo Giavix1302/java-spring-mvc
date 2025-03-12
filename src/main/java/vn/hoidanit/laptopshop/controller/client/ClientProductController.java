3706b9c22cd4e136b72253019aea813926a26bf7
Giavix1302 <nguyenhoanggiavi123@gmail.com>
2025-03-12 16:59:59 +0700

    feat: enhance product detail page by integrating product service for data retrieval

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ClientProductController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ClientProductController.java
@@ -1,16 +1,28 @@
 package vn.hoidanit.laptopshop.controller.client;
 
-import org.springframework.ui.Model;  
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
 
 @Controller
 public class ClientProductController {
 
+  private final ProductService productService;
+
+  public ClientProductController(ProductService productService) {
+    this.productService = productService;
+  }
+
   @RequestMapping("/product/{id}")
   public String getProductDetailPage(Model model, @PathVariable Long id) {
+    Product product = productService.getProductById(id).get();
+    model.addAttribute("product", product);
+    model.addAttribute("id", id);
+
     return "client/product/detail";
   }
 
